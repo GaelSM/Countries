10430c78b20e3a6ab6b13da4ff2b742e6e3158b0
Segura Mendoza Gael Alejandro <gaelsegura.mendoza@gmail.com>
2024-06-23 18:44:31 -0600

    Delete unnecessary effect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,8 @@
-import { Outlet, useLocation } from "react-router-dom"
-import { useEffect } from "react"
+import { Outlet } from "react-router-dom"
 import Header from "@components/Header"
 import "@components/App.css"
 
 export default function App() {
-  const { pathname } = useLocation()
-
-  useEffect(() => {
-    window.scroll({
-      top: 0,
-      behavior: "smooth"
-    })
-
-  }, [pathname])
-
   return (
     <>
       <Header />
